c89892a5bc996fef7e9ee9116cc3d6e107b91aa5
doannguyen <doangatesvnn93@gmail.com>
2017-02-02 17:52:17 +0700

    update

--- socket_server.js
+++ socket_server.js
@@ -11,50 +11,60 @@ var http = require('http'),
     io = socketIO.listen(server);
 // Retrieve
 
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb://root:root@ds015289.mlab.com:15289/doannguyen', function (err, db) {
-    if(err) { return console.dir(err); }
-    var collection = db.collection('slot');
-    // var data =  {monhoc:'toancc',slot:3};
-    // collection.insert(data);
-    // collection.findOne({mykey:1}, function(err, item) {});
-    var is_slot = collection.findOne({monhoc:'toancc'},function(err, item) {});
-    console.log(is_slot);
 
-    // var doc = {mykey:1, fieldtoupdate:1};
-    //
-    // collection.insert(doc, {w:1}, function(err, result) {
-    //     collection.update({mykey:1}, {$set:{fieldtoupdate:2}}, {w:1}, function(err, result) {});
-    // });
-    //
-    // var doc2 = {mykey:2, docs:[{doc1:1}]};
-    //
-    // collection.insert(doc2, {w:1}, function(err, result) {
-    //     collection.update({mykey:2}, {$push:{docs:{doc2:1}}}, {w:1}, function(err, result) {});
-    // });
-
-});
 
 io.set('match origin protocol', true);
 io.set('origins', '*:*');
 io.set('log level', 1);
-var slot = 10;
+var slot = 0;
+var test = '';
 var run = function(socket){
+    var MongoClient = require('mongodb').MongoClient;
+    MongoClient.connect('mongodb://root:root@ds015289.mlab.com:15289/doannguyen', function (err, db) {
+        if(err) { return console.dir(err); }
+
+        var collection = db.collection('slot');
+        var docs = [{mykey:1}, {mykey:2}, {mykey:3}];
+
+        collection.insert(docs, {w:1}, function(err, result) {
+
+            collection.find().toArray(function(err, items) {});
+
+            var slot_cl = collection.find({monhoc:'toancc'}).stream();
+            slot_cl.on("data", function(item) {
+                slot = item.slot;
+                console.log(item);
+                socket.emit('data',item);
+
+            });
+
+            collection.findOne({mykey:1}, function(err, item) {});
+
+        });
+
+    });
     // socket.emit('greeting', 'Hello from Socket.IO server');
     // socket.broadcast.emit('dangky', slot);
     socket.emit('soluong',slot);
+
     socket.on('dangky', function(data){
         slot = parseInt(slot) - 1;
         console.log('Slot con lai', slot);
         socket.broadcast.emit('soluong', slot);
         socket.emit('dangky',slot);
 
-    })
+    });
     socket.on('huy',function (data) {
         slot = parseInt(slot) + 1;
         socket.broadcast.emit('soluong',slot);
         socket.emit('huy',slot);
-    })
+    });
+    socket.on('data',function (data) {
+    //     console.log('co data truyen len');
+        socket.emit('data',test);
+    });
+
+
 }
 
 io.sockets.on('connection', run);
