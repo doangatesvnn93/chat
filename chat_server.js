2130862e69d54c924ac24046b66b0169ef011861
doannv1 <nguyenvandoan01@admicro.vn>
2017-08-01 17:06:17 +0700

    update

--- chat_server.js
+++ chat_server.js
@@ -13,19 +13,6 @@ io.set('match origin protocol', true);
 io.set('origins', '*:*');
 io.set('log level', 1);
 var run = function(socket){
-    // socket.emit('greeting', 'Hello from Socket.IO server');
-    socket.on('name', function(name){
-        socket.broadcast.emit('name', name);
-        socket.emit('name',name);
-    })
-    socket.on('ms', function(data){
-        socket.broadcast.emit('ms', data);
-        socket.emit('ms',data);
-    })
-    socket.on('message',function (data) {
-        socket.broadcast.emit('message',data);
-        socket.emit('message',data);
-    });
     socket.on('newMessage', function (req) {
         socket.broadcast.emit('newMessage', req);
         socket.emit('newMessage', req);
