75849fb496d95572175d49d59efacbb623c39804
doannv1 <nguyenvandoan01@admicro.vn>
2017-08-01 17:29:33 +0700

    update

--- CI/application/views/roomDetail.php
+++ CI/application/views/roomDetail.php
@@ -6,7 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php base_url(); ?>/assets/css/style.css">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css');?>"?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -74,7 +73,9 @@
                 name: name,
                 message: message
             }
-
+            if ($('#message_all').height > 300) {
+                $('#message_all').css({'overflow-y':'scroll'});
+            }
             if (message != null && message != '') {
                 socket.emit('newMessage', data);
                 $('#ms').val('');
